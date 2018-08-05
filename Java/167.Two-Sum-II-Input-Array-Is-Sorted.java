cda21ca7b3d2595154e141299ca0e1a03283902f
Fan Yang <i@fanyangcs.com>
2018-08-05 17:42:34 +0800

    Problem 167

---
+++ Java/167.Two-Sum-II-Input-Array-Is-Sorted.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int lo = 0, hi = numbers.length-1;
+        while (lo < hi) {
+            int sum = numbers[lo] + numbers[hi];
+            if (sum == target) break; 
+            else if (sum > target) hi--;
+            else lo++;
+        }
+        return new int[]{lo+1, hi+1};
+    }
+}
